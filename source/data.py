12bd8dddf3d41e323dbfb224e64e0ea73a575a5a
Pedro Ferreira <plferreira4@gmail.com>
2020-03-04 18:51:18 +0100

    Refactor the save/load functions to make it more clear the different data structures being used.

--- source/data.py
+++ source/data.py
@@ -7,6 +7,72 @@ CONFIG_PATH = Path.home() / '.config' / 'screen_ruler' / 'config.json'
 
 
 class Data():
+    @staticmethod
+    def optionsToStorage(options, currentState):
+        """
+            Convert from the 'options' structure to the 'storage' data structure.
+        """
+        storage = options.copy()
+
+        # need to get the individual rgba() from the QColor so that we can serialize it into json
+        background = storage['background_color']
+        lines = storage['lines_color']
+        divisions = storage['divisions_color']
+
+        storage['background_color'] = {
+            'red': background.red(),
+            'green': background.green(),
+            'blue': background.blue(),
+            'alpha': background.alpha()
+        }
+        storage['lines_color'] = {
+            'red': lines.red(),
+            'green': lines.green(),
+            'blue': lines.blue(),
+            'alpha': lines.alpha()
+        }
+        storage['divisions_color'] = {
+            'red': divisions.red(),
+            'green': divisions.green(),
+            'blue': divisions.blue(),
+            'alpha': divisions.alpha()
+        }
+
+        storage['ruler_width'] = currentState['width']
+        storage['ruler_height'] = currentState['height']
+        storage['options_opened'] = currentState['optionsOpened']
+
+        return storage
+
+    @staticmethod
+    def storageToOptions(storage):
+        """
+            Convert from the 'storage' structure to the 'options' data structure.
+        """
+        options = storage.copy()
+
+        # deal with the colors (from rgba values to a QColor object)
+        backgroundColor = options['background_color']
+        linesColor = options['lines_color']
+        divisionsColor = options['divisions_color']
+
+        if isinstance(backgroundColor, dict):
+            options['background_color'] = QColor(
+                backgroundColor['red'], backgroundColor['green'],
+                backgroundColor['blue'], backgroundColor['alpha'])
+
+        if isinstance(linesColor, dict):
+            options['lines_color'] = QColor(
+                linesColor['red'], linesColor['green'],
+                linesColor['blue'], linesColor['alpha'])
+
+        if isinstance(divisionsColor, dict):
+            options['divisions_color'] = QColor(
+                divisionsColor['red'], divisionsColor['green'],
+                divisionsColor['blue'], divisionsColor['alpha'])
+
+        return options
+
     def __init__(self):
         self.options = {
             'units': 'px',  # px / cm / inch
@@ -34,68 +100,21 @@ class Data():
         """
         try:
             text = CONFIG_PATH.read_text(encoding='utf-8')
-            options = json.loads(text)
+            storage = json.loads(text)
 
         except (FileNotFoundError, ValueError):
             return
 
-        for key, value in options.items():
-            self.options[key] = value
-
-            # deal with the colors (init. the QColor() from)
-        backgroundColor = self.options['background_color']
-        linesColor = self.options['lines_color']
-        divisionsColor = self.options['divisions_color']
-
-        if isinstance(backgroundColor, dict):
-            self.options['background_color'] = QColor(
-                backgroundColor['red'], backgroundColor['green'],
-                backgroundColor['blue'], backgroundColor['alpha'])
-
-        if isinstance(linesColor, dict):
-            self.options['lines_color'] = QColor(
-                linesColor['red'], linesColor['green'],
-                linesColor['blue'], linesColor['alpha'])
-
-        if isinstance(divisionsColor, dict):
-            self.options['divisions_color'] = QColor(
-                divisionsColor['red'], divisionsColor['green'],
-                divisionsColor['blue'], divisionsColor['alpha'])
+        self.options = Data.storageToOptions(storage)
 
     def save(self, currentState):
         """
             Write the current options values to the configuration file.
         """
-        # need to get the individual rgba() from the QColor so that we can serialize it into json
-        background = self.options['background_color']
-        lines = self.options['lines_color']
-        divisions = self.options['divisions_color']
-
-        self.options['background_color'] = {
-            'red': background.red(),
-            'green': background.green(),
-            'blue': background.blue(),
-            'alpha': background.alpha()
-        }
-        self.options['lines_color'] = {
-            'red': lines.red(),
-            'green': lines.green(),
-            'blue': lines.blue(),
-            'alpha': lines.alpha()
-        }
-        self.options['divisions_color'] = {
-            'red': divisions.red(),
-            'green': divisions.green(),
-            'blue': divisions.blue(),
-            'alpha': divisions.alpha()
-        }
-
-        self.options['ruler_width'] = currentState['width']
-        self.options['ruler_height'] = currentState['height']
-        self.options['options_opened'] = currentState['optionsOpened']
+        storage = Data.optionsToStorage(self.options, currentState)
 
         # make sure the directory exists before trying to create the configuration file
         if not CONFIG_PATH.parent.exists():
             CONFIG_PATH.parent.mkdir(parents=True, exist_ok=True)
 
-        CONFIG_PATH.write_text(json.dumps(self.options), encoding='utf-8')
+        CONFIG_PATH.write_text(json.dumps(storage), encoding='utf-8')
